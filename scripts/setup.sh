4b2a38f3a10925004899a82cd4a2c7b16529c663
blakebiz <57645912+blakebiz@users.noreply.github.com>
2022-04-26 12:02:03 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd ~/catkin_ws/src
-git clone https://github.com/blakebiz/robotics_report3
 git clone https://github.com/blakebiz/robotics_lab4
 git clone https://github.com/blakebiz/robotics_lab6
+# git clone https://github.com/hsaeidi-uncw/robot_vision_lectures
+# git clone https://github.com/hsaeidi-uncw/ur5e_control
 cd ..
 catkin_make
